5514cef3f80266f71e32cf2c53151ac5aa60daac
macmanes <macmanes@gmail.com>
2017-06-02 07:01:47 -0400

    for loop

--- for_loop.sh
+++ for_loop.sh
@@ -6,3 +6,12 @@ for i in $(ls -lth reads/*1.fastq.gz); do
   READ1=$F_1.fastq.gz READ2=$F_2.fastq.gz \
   LINEAGE=eukaryota SAMP=$SAMPLE --dry-run;
 done
+
+
+
+
+dataset=SRR3499127
+for i in $(ls assemblies/$dataset.1.*fasta); do
+  F=$(basename $i)
+  $HOME/Oyster_River_Protocol/orp.mk report SAMP=1 CPU=24 ASSEMBLY=$F READ1=SRR3499127_1.fastq.gz READ2=SRR3499127_2.fastq.gz LINEAGE=eukaryota --dry-run;
+done
