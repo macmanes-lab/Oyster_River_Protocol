f3f9398e143c6e1796f9b8dabfb59c9ce071133f
macmanes <macmanes@gmail.com>
2017-06-03 16:56:16 -0400

    add code for pulling out good contigs

---
+++ best_contig.sh
@@ -0,0 +1,9 @@
+input=Orthogroups.txt
+END=$(wc -l $input | awk '{print $1}')
+START=1
+
+
+for i in $(eval echo "{$START..$END}") ; do
+  sed -n ''$i'p' $input | tr ' ' '\n' | grep -f - trans/merged/contigs.csv \
+  | awk -F, '{print $1 "\t" $9}' | sort -rnk2,2 | head -1 | tee -a good.list
+done
