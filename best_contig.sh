6e6168f7f80bb03ca259568fa227cf57884a2033
Matt MacManes <macmanes@gmail.com>
2017-06-05 08:08:59 -0400

    add code for pulling out good contigs

--- best_contig.sh
+++ best_contig.sh
@@ -4,9 +4,23 @@ START=1
 
 /share/orthonucl/OrthoFinder/orthofinder/orthofinder.py -f fasta/ -og -t 40 -a 40
 
-transrate 
+cat fasta/*fasta > merged.fasta
+
+/share/test/share/transrate-1.0.4beta/transrate -o merged -t 48 \
+-a merged.fasta \
+--left ../../SRR1199463_1.fastq.gz  \
+--right ../../SRR1199463_2.fastq.gz
 
 for i in $(eval echo "{$START..$END}") ; do
   sed -n ''$i'p' $input | tr ' ' '\n' | grep -f - trans/merged/contigs.csv \
   | awk -F, '{print $1 "\t" $9}' | sort -rnk2,2 | head -1 | tee -a good.list
 done
+
+python /mnt/data3/macmanes/Mc_Transcriptome/final/Sept16/filter.py merged.fasta <(awk '{print $1}' good.list)  >  orthomerged.fasta
+
+python3 /share/busco/busco/scripts/run_BUSCO.py -i orthomerged.fasta -o mer -m tran -l $RAID/busco_dbs/eukaryota_odb9 -c 48
+
+/share/test/share/transrate-1.0.4beta/transrate -o orthomerged -t 48 \
+-a orthomerged.fasta \
+--left ../../SRR1199463_1.fastq.gz  \
+--right ../../SRR1199463_2.fastq.gz
