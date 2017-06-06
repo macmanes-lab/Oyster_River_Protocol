059f5640f7609d34feeec053a46075f68d266137
Matt MacManes <macmanes@gmail.com>
2017-06-06 13:00:28 -0400

    up

--- orp.mk
+++ orp.mk
@@ -93,10 +93,12 @@ orthofusing:
 	#cat ${DIR}/orthofuse/${DATASET}.${SAMP}/*fasta > ${DIR}/orthofuse/${DATASET}.${SAMP}/merged.fasta && \
 	#transrate -o ${DIR}/orthofuse/${DATASET}.${SAMP}/merged -t $(CPU) -a ${DIR}/orthofuse/${DATASET}.${SAMP}/merged.fasta --left ${DIR}/reads/${READ1} --right ${DIR}/reads/${READ2} && \
 	echo ${START} && \
-	echo ${END} && \
+	ORTHOINPUT=$$($${DIR}/orthofuse/$${DATASET}.$${SAMP}/Results_"date +"%b%m""/Orthogroups.txt)
+	END=$$(wc -l $$ORTHOINPUT | awk '{print $$1}')
 	echo ${ORTHOINPUT} && \
+	echo ${END} && \
 	echo ${READ1} && \
-	for i in $$(eval echo "{1..$$(wc -l $$ORTHOINPUT | awk '{print $$1}')}") ; do sed -n ''$$i'p' $$ORTHOINPUT | tr ' ' '\n' | grep -f - ${DIR}/orthofuse/${DATASET}.${SAMP}/merged/merged/contigs.csv | awk -F, 'BEGIN {max = 0} {if ($$9>max) max=$$9} END {print $$1 "\t" max}' | tee -a ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list; done && \
+	for i in $$(eval echo "{1..$$ORTHOINPUT}") ; do sed -n ''$$i'p' $$ORTHOINPUT | tr ' ' '\n' | grep -f - ${DIR}/orthofuse/${DATASET}.${SAMP}/merged/merged/contigs.csv | awk -F, 'BEGIN {max = 0} {if ($$9>max) max=$$9} END {print $$1 "\t" max}' | tee -a ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list; done && \
 	python $$(which filter.py) ${DIR}/orthofuse/${DATASET}.${SAMP}/merged.fasta <(awk '{print $$1}' ${DATASET}.${SAMP}/good.list) > ${DIR}/orthofuse/${DATASET}.${SAMP}/${DATASET}.${SAMP}.orthomerged.fasta && \
 	touch orthofuse.done
 
