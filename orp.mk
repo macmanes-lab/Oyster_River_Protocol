388b4710407e904348a980cd27e86d5f59e43c3a
Matt MacManes <macmanes@gmail.com>
2017-06-06 14:14:44 -0400

    fix END

--- orp.mk
+++ orp.mk
@@ -21,7 +21,7 @@ ASSEMBLY=
 LINEAGE=
 BUSCOUT := BUSCO_$(shell basename ${ASSEMBLY} .fasta)
 BUSCODB :=
-ORTHOINPUT ?= ${shell $${DIR}/orthofuse/$${DATASET}.$${SAMP}/Results_"date +"%b%m""/Orthogroups.txt}
+#ORTHOINPUT ?= ${shell $${DIR}/orthofuse/$${DATASET}.$${SAMP}/Results_"date +"%b%m""/Orthogroups.txt}
 END ?= ${shell wc -l $$ORTHOINPUT | awk '{print $$1}'}
 START=1
 
@@ -93,7 +93,7 @@ orthofusing:
 	#cat ${DIR}/orthofuse/${DATASET}.${SAMP}/*fasta > ${DIR}/orthofuse/${DATASET}.${SAMP}/merged.fasta && \
 	#transrate -o ${DIR}/orthofuse/${DATASET}.${SAMP}/merged -t $(CPU) -a ${DIR}/orthofuse/${DATASET}.${SAMP}/merged.fasta --left ${DIR}/reads/${READ1} --right ${DIR}/reads/${READ2} && \
 	echo ${START} && \
-	END=$$(wc -l <(find ${DIR}/orthofuse/${DATASET}.${SAMP}/ -name Orthogroups.txt 2> /dev/null) | awk '{print $$1}')
+	export END=$$(wc -l $$(find ${DIR}/orthofuse/${DATASET}.${SAMP}/ -name Orthogroups.txt 2> /dev/null) | awk '{print $$1}')
 	echo ${END} && \
 	echo ${READ1} && \
 	for i in $$(eval echo "{1..$$END}") ; do sed -n ''$$i'p' $$ORTHOINPUT | tr ' ' '\n' | grep -f - ${DIR}/orthofuse/${DATASET}.${SAMP}/merged/merged/contigs.csv | awk -F, 'BEGIN {max = 0} {if ($$9>max) max=$$9} END {print $$1 "\t" max}' | tee -a ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list; done && \
