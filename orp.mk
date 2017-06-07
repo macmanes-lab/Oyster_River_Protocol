49c8e0b40979c2b94b36228373ebc436d6d9047a
macmanes <macmanes@gmail.com>
2017-06-06 22:18:33 -0400

    up

--- orp.mk
+++ orp.mk
@@ -95,8 +95,8 @@ orthofusing:
 	export ORTHOINPUT=$$(find ${DIR}/orthofuse/${DATASET}.${SAMP}/ -name Orthogroups.txt 2> /dev/null) && \
 	#for i in $$(eval echo "{1..$$END}") ; do sed -n ''$$i'p' $$ORTHOINPUT | tr ' ' '\n' | grep -f - $$(find ${DIR}/orthofuse/${DATASET}.${SAMP}/ -name contigs.csv 2> /dev/null) | awk -F, 'BEGIN {max = 0} {if ($$9>max) max=$$9} END {print $$1 "\t" max}' | tee -a ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list; done && \
 	for i in $$(eval echo "{1..$$END}") ; do sed -n ''$$i'p' $$ORTHOINPUT | tr ' ' '\n' > $$i.txt; done && \
-	ls *txt | parallel -j $$(CPU) "grep -wf {} $$(find ${DIR}/orthofuse/${DATASET}.${SAMP}/ -name contigs.csv 2> /dev/null) > {1}.out" && \
-	ls *out | parallel -j 20 --dry-run "awk -F, 'BEGIN {max = 0} {if (\$\$9>max) max=\$\$9} END {print \$\$1 \"\\t\" max}'" | tee -a ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list && \
+	ls *txt | parallel -j $(CPU) "grep -wf {} $$(find ${DIR}/orthofuse/${DATASET}.${SAMP}/ -name contigs.csv 2> /dev/null) > {1}.out" && \
+	ls *out | parallel -j $(CPU) "awk -F, 'BEGIN {max = 0} {if (\"$$\"9>max) max=\"$$\"9} END {print \"$$\"1 \"\\t\" max}'" | tee -a ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list && \
 	python $$(which filter.py) ${DIR}/orthofuse/${DATASET}.${SAMP}/merged.fasta <(awk '{print $$1}' ${DIR}/orthofuse/${DATASET}.${SAMP}/good.list) > ${DIR}/orthofuse/${DATASET}.${SAMP}/${DATASET}.${SAMP}.orthomerged.fasta && \
 	touch orthofuse.done
 
