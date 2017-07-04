919a53f5722aed60a98a778fa41895639f3d5657
macmanes <macmanes@gmail.com>
2017-07-04 10:42:07 -0400

    PULLN OUT ORTHOFUSE

--- oyster.mk
+++ oyster.mk
@@ -4,7 +4,8 @@ SHELL=/bin/bash -o pipefail
 
 #USAGE:
 #
-#	oyster.mk prep main READ1= READ2= MEM=500 CPU=24 RUNOUT=runname
+#	oyster.mk main READ1= READ2= MEM=500 CPU=24 RUNOUT=runname
+# oyster.mk orthofuse FASTADIR= READ1= READ2= MEM=500 CPU=24 RUNOUT=runname
 #
 
 MAKEDIR := $(dir $(firstword $(MAKEFILE_LIST)))
@@ -35,16 +36,12 @@ run_shannon:${DIR}/assemblies/${RUNOUT}.shannon.fasta
 merge:${DIR}/orthofuse/${RUNOUT}/merged.fasta
 orthotransrate:${DIR}/orthofuse/${RUNOUT}/orthotransrate.done
 orthofusing:${DIR}/assemblies/${RUNOUT}.orthomerged.fasta
-merge2:${DIR}/orthofuse/${RUNOUT}/merged.fasta
-orthotransrate2:${DIR}/orthofuse/${RUNOUT}/orthotransrate.done
-orthofusing2:${DIR}/assemblies/${RUNOUT}.orthomerged.fasta
 
 
 main: setup run_trimmomatic run_rcorrector run_trinity run_spades75 run_spades55 run_shannon merge orthotransrate orthofusing report
 report:busco transrate reportgen
 busco:${DIR}/reports/busco.done
 transrate:${DIR}/reports/transrate.done
-orthofuse:merge2 orthotransrate2 orthofusing2
 
 .DELETE_ON_ERROR:
 .PHONY:report
@@ -110,48 +107,6 @@ ${DIR}/assemblies/${RUNOUT}.orthomerged.fasta:${DIR}/orthofuse/${RUNOUT}/orthotr
 	python ${MAKEDIR}/scripts/filter.py ${DIR}/orthofuse/${RUNOUT}/merged.fasta ${DIR}/orthofuse/${RUNOUT}/good.list > ${DIR}/orthofuse/${RUNOUT}/${RUNOUT}.orthomerged.fasta
 	cp ${DIR}/orthofuse/${RUNOUT}/${RUNOUT}.orthomerged.fasta ${DIR}/assemblies/${RUNOUT}.orthomerged.fasta
 	rm ${DIR}/orthofuse/${RUNOUT}/good.list
-
-
-
-
-
-
-
-
-${DIR}/orthofuse/${RUNOUT}/merged.fasta:
-	mkdir -p ${DIR}/orthofuse/${RUNOUT}
-	for fasta in $$(ls ${FASTADIR}); do python ${MAKEDIR}/scripts/long.seq.py ${FASTADIR}/$$fasta ${FASTADIR}/$$fasta.short.fasta 200;gzip  ${FASTADIR}/$$fasta ; done
-	python $$(which orthofuser.py) -I 4 -f ${FASTADIR} -og -t $(CPU) -a $(CPU)
-	cat ${FASTADIR}/*short.fasta > ${DIR}/orthofuse/${RUNOUT}/merged.fasta
-
-${DIR}/orthofuse/${RUNOUT}/orthotransrate.done:${DIR}/orthofuse/${RUNOUT}/merged.fasta
-	export END=$$(wc -l $$(find ${FASTADIR} -name Orthogroups.txt 2> /dev/null) | awk '{print $$1}') && \
-	export ORTHOINPUT=$$(find ${FASTADIR} -name Orthogroups.txt 2> /dev/null) && \
-	parallel  -j $(CPU) -k "sed -n ''{}'p' $$ORTHOINPUT | tr ' ' '\n' | sed '1d' > ${DIR}/orthofuse/${RUNOUT}/{1}.groups"  ::: $$(eval echo "{1..$$END}")
-	transrate -o ${DIR}/orthofuse/${RUNOUT}/merged -t $(CPU) -a ${DIR}/orthofuse/${RUNOUT}/merged.fasta --left ${READ1} --right ${READ2}
-	touch ${DIR}/orthofuse/${RUNOUT}/orthotransrate.done
-
-${DIR}/assemblies/${RUNOUT}.orthomerged.fasta:${DIR}/orthofuse/${RUNOUT}/orthotransrate.done
-	echo All the text files are made, start GREP
-	find ${DIR}/orthofuse/${RUNOUT}/ -name *groups 2> /dev/null | parallel -j $(CPU) "grep -wf {} $$(find ${DIR}/orthofuse/${RUNOUT}/ -name contigs.csv 2> /dev/null) > {1}.orthout 2> /dev/null"
-	echo About to delete all the text files
-	find ${DIR}/orthofuse/${RUNOUT}/ -name *groups -delete
-	echo Search output files
-	find ${DIR}/orthofuse/${RUNOUT}/ -name *orthout 2> /dev/null | parallel -j $(CPU) "awk -F, 'BEGIN {max = 0} {if (\$$9>max) max=\$$9} END {print \$$1 \"\\t\" max}'" | tee -a ${DIR}/orthofuse/${RUNOUT}/good.list
-	find ${DIR}/orthofuse/${RUNOUT}/ -name *orthout -delete
-	python ${MAKEDIR}/scripts/filter.py ${DIR}/orthofuse/${RUNOUT}/merged.fasta <(awk '{print $$1}' ${DIR}/orthofuse/${RUNOUT}/good.list) > ${DIR}/orthofuse/${RUNOUT}/${RUNOUT}.orthomerged.fasta
-	cp ${DIR}/orthofuse/${RUNOUT}/${RUNOUT}.orthomerged.fasta ${DIR}/assemblies/${RUNOUT}.orthomerged.fasta
-	rm ${DIR}/orthofuse/${RUNOUT}/good.list
-
-
-
-
-
-
-
-
-
-
 
 ${DIR}/reports/busco.done:${DIR}/assemblies/${RUNOUT}.orthomerged.fasta
 	python3 $$(which run_BUSCO.py) -i ${DIR}/assemblies/${RUNOUT}.orthomerged.fasta -m transcriptome --cpu $(CPU) -l ${LINEAGE} -o ${RUNOUT}
