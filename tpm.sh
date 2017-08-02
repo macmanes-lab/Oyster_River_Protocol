e00076dbac17b2fc48158a13e84a6be18c0832cc
Matt MacManes <macmanes@gmail.com>
2017-08-02 12:41:32 -0400

    quants

--- tpm.sh
+++ tpm.sh
@@ -4,25 +4,25 @@ cat buscos.txt | sed -n 1,10p | cut -f2
 echo $1
 for i in $(sed -n 1,10p buscos.txt | cut -f3)
 do
-    grep $i $1 | cut -f4
+    grep $i $1/quant.sf | cut -f4
 done
 
 echo $2
 for i in $(sed -n 11,20p buscos.txt | cut -f3)
 do
-    grep $i $2 | cut -f4
+    grep $i $2/quant.sf | cut -f4
 done
 
 echo $3
 for i in $(sed -n 21,30p buscos.txt | cut -f3)
 do
-    grep $i $3 | cut -f4
+    grep $i $3/quant.sf | cut -f4
 done
 
 echo $4
 for i in $(sed -n 31,40p buscos.txt | cut -f3)
 do
-    grep $i $4 | cut -f4
+    grep $i $4/quant.sf | cut -f4
 done
 
 rm buscos.txt
