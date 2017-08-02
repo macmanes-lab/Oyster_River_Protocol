74ead47cd7036ffd4a2863bc8f087a18ca928801
Matt MacManes <macmanes@gmail.com>
2017-08-02 12:07:32 -0400

    update

--- tpm.sh
+++ tpm.sh
@@ -5,18 +5,24 @@ echo $1
 for i in $(sed -n 1,10p buscos.txt | cut -f3)
 do
     grep $i $1 | cut -f4
-done 
+done
 
 echo $2
 for i in $(sed -n 11,20p buscos.txt | cut -f3)
 do
     grep $i $2 | cut -f4
-done 
+done
 
 echo $3
 for i in $(sed -n 21,30p buscos.txt | cut -f3)
 do
     grep $i $3 | cut -f4
+done
+
+echo $4
+for i in $(sed -n 31,40p buscos.txt | cut -f3)
+do
+    grep $i $4 | cut -f4
 done
 
 rm buscos.txt
