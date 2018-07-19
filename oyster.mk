d25c00d677462d30c28bed11ad76db3a410aa3bf
macmanes <macmanes@gmail.com>
2018-07-18 21:21:07 -0400

    update

--- oyster.mk
+++ oyster.mk
@@ -209,6 +209,7 @@ ${DIR}/assemblies/shmlast/${RUNOUT}.newbies.fasta:${DIR}/assemblies/shmlast/${RU
 	cd ${DIR}/assemblies/shmlast/ && rm -f ${RUNOUT}.list*
 
 ${DIR}/reports/busco.done:${DIR}/assemblies/${RUNOUT}.orthomerged.fasta
+	export BUSCO_CONFIG_FILE=${MAKEDIR}/software/config.ini 
 	python $$(which run_BUSCO.py) -i ${DIR}/assemblies/${RUNOUT}.orthomerged.fasta -m transcriptome --cpu $(CPU) -o ${RUNOUT}.orthomerged
 	mv run_${RUNOUT}* ${DIR}/reports/
 	touch ${DIR}/reports/${RUNOUT}.busco.done
