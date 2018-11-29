2fa9e8a10b66b4702740312890c25b54f3530fe8
Matthew MacManes <macmanes@matthews-pro.ad.unh.edu>
2018-11-29 11:02:01 -0500

    new lines

--- oyster.mk
+++ oyster.mk
@@ -149,8 +149,8 @@ ifeq ($(shell file ${READ1} | awk '{print $$2}'),gzip)
 	then\
 		printf " ";\
 	else\
-		printf "\n\n\n\n IT LOOKS LIKE YOUR READS ARE NOT AT LEAST 75 BP LONG, ";\
-		printf "PLEASE EDIT YOUR COMMAND USING THE "SPADES2_KMER=INT" FLAGS,";\
+		printf "\n\n\n\n IT LOOKS LIKE YOUR READS ARE NOT AT LEAST 75 BP LONG,\n ";\
+		printf "PLEASE EDIT YOUR COMMAND USING THE "SPADES2_KMER=INT" FLAGS,\n";\
 		printf "SETTING THE ASSEMBLY LENGTH LESS THAN YOUR READ LENGTH \n\n\n\n";\
 		$$(shell exit);\
 	fi
@@ -159,8 +159,8 @@ else
 	then\
 		printf " ";\
 	else\
-		printf "\n\n\n\n IT LOOKS LIKE YOUR READS ARE NOT AT LEAST 75 BP LONG, ";\
-		printf "PLEASE EDIT YOUR COMMAND USING THE "SPADES2_KMER=INT" FLAGS,";\
+		printf "\n\n\n\n IT LOOKS LIKE YOUR READS ARE NOT AT LEAST 75 BP LONG,\n ";\
+		printf "PLEASE EDIT YOUR COMMAND USING THE "SPADES2_KMER=INT" FLAGS,\n";\
 		printf "SETTING THE ASSEMBLY LENGTH LESS THAN YOUR READ LENGTH \n\n\n\n";\
 		$$(shell exit);\
 	fi
